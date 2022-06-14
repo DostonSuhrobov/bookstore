6f985ca1ed1f06011817c2bc30d4efc3a336afb4
DostonSuhrobov <doston.sanatovich@gmail.com>
2022-06-14 18:29:24 -0400

    bookstore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { BooklistComponent } from './booklist/booklist.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooklistComponent,
+    WishlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
